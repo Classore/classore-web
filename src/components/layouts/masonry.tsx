26df8616b3be57fcbc2decd8732947d5934b8ae1
Samson Okunola <smsnmichea@gmail.com>
2025-02-11 19:33:48 +0100

    updates

--- src/components/layouts/masonry.tsx
+++ src/components/layouts/masonry.tsx
@@ -1,15 +1,26 @@
-import Masonry from "react-responsive-masonry";
+import Masonry from "react-masonry-css";
 import React from "react";
+
+import { cn } from "@/lib";
 
 interface Props {
 	children: React.ReactNode;
-	columnsCount?: number;
-	gutter?: string;
+	breakpointCols?: number;
+	className?: string;
+	columnClassName?: string;
 }
 
-export function MasonryLayout({ children, columnsCount = 3, gutter = "20px" }: Props) {
+export function MasonryLayout({
+	children,
+	breakpointCols = 3,
+	className,
+	columnClassName,
+}: Props) {
 	return (
-		<Masonry columnsCount={columnsCount} gutter={gutter}>
+		<Masonry
+			className={cn("flex w-auto flex-wrap", className)}
+			columnClassName={cn("flex flex-col gap-4 pl-5", columnClassName)}
+			breakpointCols={breakpointCols}>
 			{children}
 		</Masonry>
 	);
