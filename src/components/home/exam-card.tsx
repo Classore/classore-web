edc5216700176b8b5235db9f9e33e8445a32d0a8
Samson Okunola <smsnmichea@gmail.com>
2025-03-13 20:14:06 +0100

    updates

--- src/components/home/exam-card.tsx
+++ src/components/home/exam-card.tsx
@@ -1,61 +1,65 @@
-import { formatCurrency, formatNumber } from '@/lib'
-import type { ExamBundlesResp } from '@/queries/school'
-import { RiStarFill, RiTimeLine, RiUserLine } from '@remixicon/react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { twMerge } from 'tailwind-merge'
+import { formatCurrency, formatNumber } from "@/lib";
+import type { ExamBundlesResp } from "@/queries/school";
+import { RiStarFill, RiTimeLine, RiUserLine } from "@remixicon/react";
+import Image from "next/image";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
-	course: ExamBundlesResp['data'][number]
-	className?: string
+  course: ExamBundlesResp["data"][number];
+  className?: string;
 }
 
 export const ExamCard = ({ course, className }: Props) => {
-	console.log('course', course)
-	return (
-		<Link
-			href={`/dashboard/categories/${course.examinationbundle_id}`}
-			className={twMerge(
-				'flex flex-col gap-3 rounded-lg border bg-white p-2.5 transition-all duration-700 hover:shadow-2xl md:p-4',
-				className
-			)}>
-			<Image
-				src={
-					course.examinationbundle_banner ??
-					'https://images.unsplash.com/photo-1446329360995-b4642a139973?q=80&w=1977&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-				}
-				alt={course.examinationbundle_name}
-				width={318}
-				height={172}
-				className='h-52 w-full rounded-md object-cover'
-			/>
-			<div className='flex items-center justify-between gap-1'>
-				<h5 className='font-medium capitalize'>
-					{course.examinationbundle_name.toUpperCase()} Prep Bundle
-				</h5>
+  console.log("course", course);
+  return (
+    <Link
+      href={`/dashboard/categories/${course.examinationbundle_id}`}
+      className={twMerge(
+        "flex flex-col gap-3 rounded-lg border bg-white p-2.5 transition-all duration-700 hover:shadow-2xl md:p-4",
+        className,
+      )}
+    >
+      <Image
+        src={
+          course.examinationbundle_banner ??
+          "https://images.unsplash.com/photo-1446329360995-b4642a139973?q=80&w=1977&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+        alt={course.examinationbundle_name}
+        width={318}
+        height={172}
+        className="h-52 w-full rounded-md object-cover"
+      />
+      <div className="flex items-center justify-between gap-1">
+        <h5 className="font-medium capitalize">
+          {course.examinationbundle_name.toUpperCase()} Prep Bundle
+        </h5>
 
-				<p className='text-sm font-bold'>
-					{formatCurrency(course.examinationbundle_amount)}
-				</p>
-			</div>
+        <p className="text-sm font-bold">
+          {formatCurrency(course.examinationbundle_amount)}
+        </p>
+      </div>
 
-			<div className='flex items-center gap-4 border-t border-neutral-200 pt-3 text-neutral-400'>
-				<div className='flex items-center gap-1'>
-					<RiTimeLine size={18} />
-					<span className='text-sm'>15 hrs</span>
-				</div>
-				<div className='flex items-center gap-1'>
-					<RiUserLine size={18} />
-					<span className='text-sm'>{formatNumber(course.enrolled)} Enrolled</span>
-				</div>
+      <div className="flex items-center gap-4 border-t border-neutral-200 pt-3 text-neutral-400">
+        <div className="flex items-center gap-1">
+          <RiTimeLine size={18} />
+          <span className="text-sm">15 hrs</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <RiUserLine size={18} />
+          <span className="text-sm">
+            {formatNumber(course.enrolled)} Enrolled
+          </span>
+        </div>
 
-				<div className='ml-auto flex items-center gap-1'>
-					<RiStarFill className='size-4 text-yellow-500' />
-					<p className='text-sm'>
-						{course.examinationbundle_rating} ({formatNumber(course.raters ?? 0)})
-					</p>
-				</div>
-			</div>
-		</Link>
-	)
-}
+        <div className="ml-auto flex items-center gap-1">
+          <RiStarFill className="size-4 text-yellow-500" />
+          <p className="text-sm">
+            {course.examinationbundle_rating} (
+            {formatNumber(course.raters ?? 0)})
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
--- src/store/z-store/auth.ts
+++ src/store/z-store/auth.ts
@@ -1,12 +1,12 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface AuthStore {
-	tempToken: string | null
-	setTempToken: (token: string | null) => void
+  tempToken: string | null;
+  setTempToken: (token: string | null) => void;
 }
 
 // Store for temporary token during
-export const useTempAuthStore = create<AuthStore>(set => ({
-	tempToken: null,
-	setTempToken: (token: string | null) => set({ tempToken: token }),
-}))
+export const useTempAuthStore = create<AuthStore>((set) => ({
+  tempToken: null,
+  setTempToken: (token: string | null) => set({ tempToken: token }),
+}));
