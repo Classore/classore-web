3371f7b6fdf445bab65be1d2470a188971720a4f
Samson Okunola <smsnmichea@gmail.com>
2025-03-28 18:49:01 +0100

    chore: added ward details

---
+++ src/pages/parents/dashboard/messages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ParentDashboardLayout } from "@/components/layouts";
+
+const Page = () => {
+	return (
+		<ParentDashboardLayout title="Messages">
+			<div className="h-full w-full"></div>
+		</ParentDashboardLayout>
+	);
+};
+
+export default Page;
